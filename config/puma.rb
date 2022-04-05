d8a4bdb33d2875e0dd03f7ce74c86457997f9275
Herbert Joseph <techniquea2z@gmail.com>
2022-04-05 02:38:56 -0400

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -13,6 +13,10 @@ threads min_threads_count, max_threads_count
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
+if ENV.fetch("RAILS_ENV", "development") == "production"
+  bind "unix:///home/cloudsigma/fe-repo/shared/tmp/sockets/app_name-puma.sock"
+end
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port ENV.fetch("PORT") { 3000 }
