33b4a9bed2b560ae28bde40156d5e0a588c2fe42
Herbert Joseph <techniquea2z@gmail.com>
2022-04-05 00:35:38 -0400

    typo

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,5 +2,5 @@ class Collection < ApplicationRecord
   belongs_to :user
 
   has_many :asset_collections
-  has_many :assets, thorugh: :asset_collections
+  has_many :assets, through: :asset_collections
 end
